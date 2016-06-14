53710c008bfefdb67e1941ca70f867e96c5254e6
AlbertPotter <smilepotter0128@gmail.com>
2016-06-14 12:43:56 -0700

    Change to request.original_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -11,7 +10,7 @@ User.create!(name:  "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name,
+  User.create!(name:  name,
               email: email,
               password:              password,
               password_confirmation: password,
@@ -19,17 +18,8 @@ User.create!(name:  "Example User",
               activated_at: Time.zone.now)
 end
 
-# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
-
-# Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
