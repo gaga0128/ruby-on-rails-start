872ea23e9ee91090b23038e82410c3cee41571e7
AlbertPotter <smilepotter0128@gmail.com>
2016-06-14 12:43:56 -0700

    Change to request.original_url

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,5 @@
 class StaticPagesController < ApplicationController
-
   def home
-    if logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    end
   end
 
   def help
