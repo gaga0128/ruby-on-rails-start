2a04a1d86d1d9eef419eb9bd9d1531bc3550d35b
AlbertPotter <smilepotter0128@gmail.com>
2017-08-28 19:51:24 -0700

    Add jQuery

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -9,13 +9,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.first.toggle!(:activated)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    assigns(:users).each do |user|
-      assert user.activated?
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
